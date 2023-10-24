7fa0a54af0dfc0aa713645e4969362ec642ed05d
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-24 14:30:45 -0500

    Primer commit

---
+++ Examen.c
@@ -0,0 +1,19 @@
+#include <stdio.h> // se agrega la biblioteca que se va utilizar//
+int main(void) { // se ingresa la funcion principal main//
+     
+    int N; // aqui se ingresan las variables que se utilizaran a lo largo del problema//
+    int promedio;
+    int mayor;
+    int menor;
+    float sum = 0.0; // se ingresa una variable la cual va tener decimales por eso se le asigna el valor de 0.0//
+
+    printf ("Ingrese un numero entero");
+    scanf("%d", &N);
+
+    
+
+    
+
+
+    return 0; // es el valor que el programa regresa si esta bien ejecutado//
+}
