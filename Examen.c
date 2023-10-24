1ed612e74c651fddfc956d9498f5f16504102874
Andradeudla <daniel.andrade.rosillo@udla.edu.ec>
2023-10-24 14:38:32 -0500

    segundo commit

--- Examen.c
+++ Examen.c
@@ -4,15 +4,18 @@ int main(void) { // se ingresa la funcion principal main//
     int N; // aqui se ingresan las variables que se utilizaran a lo largo del problema//
     int promedio;
     int mayor;
+    int num;
     int menor;
+    int i;
     float sum = 0.0; // se ingresa una variable la cual va tener decimales por eso se le asigna el valor de 0.0//
 
     printf ("Ingrese un numero entero");
     scanf("%d", &N);
 
-    
-
-    
+    printf("Introduce %d números: ", N);
+    for(int i = 0; i < N; i++) { // se realiza un bucle for para que se repita un numero exacto de veces//
+        scanf("%d", &num); // se escanean los valores//
+        sum += num; // Agrega el número a la suma total//    
 
 
     return 0; // es el valor que el programa regresa si esta bien ejecutado//
